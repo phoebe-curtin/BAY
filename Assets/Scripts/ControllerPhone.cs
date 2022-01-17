37dd8b9ccfe9c123d068882e66b264863e33da5d
JOSEPH QUESTED <josephquested@gmail.com>
2022-01-17 20:39:46 +1300

    cell phone tab

--- Assets/Scripts/ControllerPhone.cs
+++ Assets/Scripts/ControllerPhone.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ControllerPhone : MonoBehaviour
+public class PhoneHome : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
