51d88a4306f4311706f9344c9916557f837035c3
Joseph Quested <josephquested@gmail.com>
2022-01-22 13:25:02 +1300

    island spawning better

--- Assets/Scripts/IslandGenerator.cs
+++ Assets/Scripts/IslandGenerator.cs
@@ -21,6 +21,7 @@ public class IslandGenerator : MonoBehaviour
             Vector3 spawnPos = new Vector3(Random.Range(bottomLeftTrans.position.x, topRightTrans.position.x), 0.5f, Random.Range(bottomLeftTrans.position.z, topRightTrans.position.z));
             GameObject islandObj = Instantiate(islandPrefab, spawnPos, transform.rotation);
             islandObj.transform.parent = transform.parent;
+            islandObj.GetComponent<CapsuleCollider>().radius = Random.Range(0.25f, 0.75f);
             islandObj.GetComponent<Island>().RandomizeSprites();
             numberOfIslands--;
         }
